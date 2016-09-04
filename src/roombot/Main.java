7603a6fa1465e8f0429c7f25ddba59f6fd846f3b
kbowen99 <kurtisbowen99@gmail.com>
2016-09-04 16:29:24 -0500

    COM Selection

--- src/roombot/Main.java
+++ src/roombot/Main.java
@@ -1,6 +1,7 @@
 package roombot;
 
 import java.net.InetAddress;
+import java.util.Scanner;
 
 /**
  * Main class is used to construct & pass information between Webserver & Serial Port
@@ -8,13 +9,18 @@ import java.net.InetAddress;
  */
 public class Main {
 	static String ipAddr; //Local IP Address, put in main for future expansion
+	static String COM_PORT;
+	static Scanner sc = new Scanner(System.in);
+	
 	//static SerialControl s; //Serial Control, is accessed by Webserver
 	//static WebServr w; //Webserver (Its an ugly mess)
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {	
+		System.out.println("COM Port:");
+		COM_PORT = sc.next();
 		ipAddr = InetAddress.getLocalHost().getHostAddress().toString(); //Get Local
-		SerialControl s = new SerialControl("COM3", 9600); //"COM3" should be found automatically... TODO: Find Serial Port to use
+		SerialControl s = new SerialControl(COM_PORT, 9600); //"COM3" should be found automatically... TODO: Find Serial Port to use
 		WebServr w = new WebServr(80, s); //Start Webserver on Port 80
 	}
 	/**
